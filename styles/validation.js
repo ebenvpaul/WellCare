00014abfcc94584b9e9f3927729d16c405b5f287
Eben Varghese Paul <eben92paul@gmail.com>
2022-04-23 13:55:55 +0530

    Email

--- styles/validation.js
+++ styles/validation.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
         $('#send_message').click(function(e){
             
             //Stop form submission & check the validation
-            e.preventDefault();
+            // e.preventDefault();
             
             // Variable declaration
             var error = false;
@@ -42,25 +42,25 @@ $(document).ready(function(){
             }
             
             // If there is no validation error, next to process the mail function
-            if(error == false){
-               // Disable submit button just after the form processed 1st time successfully.
-                $('#send_message').attr({'disabled' : 'true', 'value' : 'Sending...' });
+            // if(error == false){
+            //    Disable submit button just after the form processed 1st time successfully.
+            //     $('#send_message').attr({'disabled' : 'true', 'value' : 'Sending...' });
                 
-				/* Post Ajax function of jQuery to get all the data from the submission of the form as soon as the form sends the values to email.php*/
-                $.post("email.php", $("#contact_form").serialize(),function(result){
-                    //Check the result set from email.php file.
-                    if(result == 'sent'){
-                        //If the email is sent successfully, remove the submit button
-                         $('#submit').remove();
-                        //Display the success message
-                        $('#mail_success').fadeIn(500);
-                    }else{
-                        //Display the error message
-                        $('#mail_fail').fadeIn(500);
-                        // Enable the submit button again
-                        $('#send_message').removeAttr('disabled').attr('value', 'Send The Message');
-                    }
-                });
-            }
+			// 	/* Post Ajax function of jQuery to get all the data from the submission of the form as soon as the form sends the values to email.php*/
+            //     $.post("email.php", $("#contact_form").serialize(),function(result){
+            //         //Check the result set from email.php file.
+            //         if(result == 'sent'){
+            //             //If the email is sent successfully, remove the submit button
+            //              $('#submit').remove();
+            //             //Display the success message
+            //             $('#mail_success').fadeIn(500);
+            //         }else{
+            //             //Display the error message
+            //             $('#mail_fail').fadeIn(500);
+            //             // Enable the submit button again
+            //             $('#send_message').removeAttr('disabled').attr('value', 'Send The Message');
+            //         }
+            //     });
+            // }
         });    
     });
